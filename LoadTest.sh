cba8219625c53a1b3a638c756e84e6f967354bfc
Dennis Mårtensson <dennismartensson@gmail.com>
2012-07-05 10:35:47 -0400

    added bash script and generation of loadtime diagram in html

--- LoadTest.sh
+++ LoadTest.sh
@@ -2,9 +2,13 @@
 echo "Runing load test on:" $1 " with " $2 " connections and " $3 " clients"
 
 COUNTER=0
-
+rm data.ph
+rm finichetInstances.ph
 while [  $COUNTER -lt $3 ]; do
-             phantomjs ph.js $1 $2 &
+             phantomjs ph.js $1 $2 $3 &
              let COUNTER=COUNTER+1 
          done
          wait
+         node createLog.js
+         open charting.html
+         killall -9 phantomjs  
--- ph.js
+++ ph.js
@@ -1,71 +1,75 @@
 var fs = require('fs');
 var workingDirectory = fs.workingDirectory;
 
+var dataFile = '/data.ph'
+var finichetFile = '/finichetInstances.ph'
+
 var pages = [];
+var times = [];
+
+var numberOfErrors = 0;
 
 
 
 if (phantom.args.length === 0) {
-	    console.log('Usage: loadspeed.js <some URL>');
-	    phantom.exit();
+	console.log('Usage: loadspeed.js <some URL>');
+	phantom.exit();
 } else {
 	var address = phantom.args[0];
 	var nrConected = phantom.args[1];
 	var totalNrConected = nrConected;
+	var totalNrOfClients = phantom.args[2];
 	process();
 }
 
 
 function process() {
-	if(nrConected > 0) {
-	 	nrConected = nrConected-1;
-	 	console.log('Number of conected ' +  (totalNrConected - nrConected));
-	   	loadSite(address, process);
-	}else {
+	if (nrConected > 0) {
+		nrConected = nrConected - 1;
+		console.log('Number of conected ' + (totalNrConected - nrConected));
+		loadSite(address, process);
+	} else {
 		runOnAll();
-		phantom.exit();
+		writeDataFile();
+		writeFinichetFile();
+		redyToKill();
 	}
 }
 
 
 function loadSite(address, callback) {
 	t = Date.now();
-    
-    var page = require('webpage').create(), t, address;
 
-    pages.push(page);
+	var page = require('webpage').create(),
+		t, address;
 
-    page.open(address, function (status) {
-    	if (status !== 'success') {
-            console.log('FAIL to load the address');
-        } else {
-            t = Date.now() - t;
-            console.log('Loading time ' + t + ' msec');
+	pages.push(page);
 
-           page.evaluate(function() {
-				var ssAppName = 'demo';
-				var ssFuncToTest = ['LoadTest'];
-				var numberOfLoops = 0;
+	page.open(address, function(status) {
+		if (status !== 'success') {
+			console.log('FAIL to load the address');
+		} else {
+			t = Date.now() - t;
+			console.log('Loading time ' + t + ' msec');
+			times.push(t);
+		}
 
-				for(var x=0; x < numberOfLoops; x++){
-					for(var i=0; i < ssAppName.length; i++){
-						if(typeof ssFuncToTest[i] !== "undefined")
-						{
-					   		ss.rpc(ssAppName + '.' + ssFuncToTest[i]);
-						}
-					}
-				}
-			});
-        }
+		callback.apply();
+	});
 
-        callback.apply();
-    });
-
-    page.onConsoleMessage = function(msg) {
+	page.onError = function (msg, trace) {
+		numberOfErrors++;
+		console.log(msg);
+		trace.forEach(function(item) {
+			console.log('  ', item.file, ':', item.line);
+		})
+	}
+		
+	page.onConsoleMessage = function(msg) {
 		console.log(msg);
 	};
 
-	/*
+/*
 	page.onResourceRequested = function (request) {
 	   	console.log('Request ' + JSON.stringify(request, undefined, 4));
 	};
@@ -73,21 +77,61 @@ function loadSite(address, callback) {
 	page.onResourceReceived = function (response) {
 		console.log('Receive ' + JSON.stringify(response, undefined, 4));
 	};
-	*/
+*/
 }
 
 
 function runOnAll() {
-	for(var z=0; z < pages.length; z++){
-		console.log('ss.rpc call: ' + z);
-		
-		pages[z].evaluate(function() {
-			var ssAppName = 'demo';
-			var ssFuncToTest = ['LoadTest'];
-			ss.rpc(ssAppName + '.' + ssFuncToTest[0]);
-		});
+	for (var z = 0; z < pages.length; z++) {
+		try {
+			pages[z].evaluate(function() {
+				var ssAppName = 'demo';
+				var ssFuncToTest = 'LoadTest';
+				ss.rpc(ssAppName + '.' + ssFuncToTest);
+			});
+		} catch (e) {
+			console.log(e);
+		}
 	}
 }
 
 
+function redyToKill() {
+	var nrDone = getRunningProceses();
+	if (nrDone >= totalNrOfClients) {
+		console.log('------------EXIT------------' + 'numberOfErrors: ' + numberOfErrors);
+		phantom.exit();
+	} else {
+		setTimeout(redyToKill(), 1000);
+	}
+}
+
+
+function getRunningProceses() {
+	fg = fs.read(workingDirectory + finichetFile);
+	var array = fg.toString().split("\n");
+	return array.length -1
+}
+
+
+function writeDataFile() {
+	try {
+			fd = fs.open(workingDirectory + dataFile, 'a');
+			fd.writeLine('[' + times + ']');
+			fd.close();
+		} catch (e) {
+			console.log(e);
+		}
+}
+
+
+function writeFinichetFile() {
+	try {
+			ff = fs.open(workingDirectory + finichetFile, 'a');
+			ff.writeLine('Done');
+			ff.close();
+		} catch (e) {
+			console.log(e);
+		}
+}
 
